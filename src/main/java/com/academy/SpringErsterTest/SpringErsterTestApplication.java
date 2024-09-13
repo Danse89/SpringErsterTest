b25b023f8d2f4d963beba241ab082f194b99d7df
Danse89 <richard.cheese89@googlemail.com>
2024-09-13 10:42:29 +0200

    ERster Test

---
+++ src/main/java/com/academy/SpringErsterTest/SpringErsterTestApplication.java
@@ -0,0 +1,13 @@
+package com.academy.SpringErsterTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringErsterTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringErsterTestApplication.class, args);
+	}
+
+}
